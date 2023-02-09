2d9d99edf96d9306efdc4020107c00cedd3da3cc
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-09 23:19:47 +0900

    feat: Update Study Planner project

---
+++ studyPlanner/src/test/java/com/hyunsb/studyPlanner/StudyPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyunsb.studyPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
