621623e7496e2b0f2862199089b30d60eee684d5
정현수 <96504592+hyunsb@users.noreply.github.com>
2023-02-18 01:10:22 +0900

    feat: Update PlannerController - 로그인 시 세션 값 확인 후 페이지 이동

--- studyPlanner/src/main/java/com/hyunsb/studyPlanner/controller/PlannerController.java
+++ studyPlanner/src/main/java/com/hyunsb/studyPlanner/controller/PlannerController.java
@@ -2,13 +2,20 @@ package com.hyunsb.studyPlanner.controller;
 
 import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PlannerController {
 
     @GetMapping("/planner/home")
-    public String home(Session session){
+    public String home(Model model, HttpServletRequest request){
+
+        if(request.getSession() != null)
+            model.addAttribute("sessionValue", true);
+
         return "/planner/home";
     }
 
